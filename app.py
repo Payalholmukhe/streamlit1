c900ee3fa5a385687d94e3463fb99f1eeb135bb2
Payalholmukhe <holmukhepayal@gmail.com>
2025-05-21 11:13:35 +0000

    123

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-import requests
-import pandas as pd
-import plotly.graph_objects as go
-import streamlit as st
-from client import StockApi
- 
- 
- 
- 
-## add page title
- 
-st.set_page_config(page_title= "Stock Market App"  , layout= "wide")
-
-
-## add title for page
-st.title("Stock market app")
-
-## add subheading
-st.subheader("By Payal Holmukhe")
-
-## add box for company
-company =st.text_input("company name")
-
-
-
-
-
-
 import streamlit as st
 from client import STOCK_API
 import plotly.graph_objects as go
